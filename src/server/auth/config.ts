2aca4668be4d58fe7ccaa1cd2d0a85a958434c75
Rohan Bera <rohanbera4002@gmail.com>
2025-01-15 17:42:33 +0530

    Database Structed - 1

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,8 +1,7 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-import { db } from "@/server/db";
+import { env } from "@/env";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -31,19 +30,11 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
-  providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
-  adapter: PrismaAdapter(db),
+  providers: [DiscordProvider],
+  secret: env.AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
