033c2f87bc07058a42f251d63edff26c18fc94d1
Rohan Bera <rohanbera4002@gmail.com>
2025-01-24 12:07:05 +0530

    Post grid hover effect added

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,32 +1,39 @@
+"use client";
+
 import { AiOutlineMessage } from "react-icons/ai";
 import { AiFillMessage } from "react-icons/ai";
 import { GoHeart } from "react-icons/go";
 import { GoHeartFill } from "react-icons/go";
 import NavLink from "@/components/navbar/NavLink";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function AppHeader() {
+  const pathname = usePathname();
+
   return (
     <header className="sticky left-0 top-0 z-50 flex h-14 w-full items-center justify-between border-b-[1px] border-gray-600 bg-black px-4 shadow-lg lg:hidden">
-      <Link href={"#"} className="inline-flex">
+      <Link href={"/"} className="inline-flex">
         <h1 className="text-3xl">Scroll</h1>
       </Link>
       <ul className="flex items-center gap-x-4">
         <li>
           <NavLink
-            linkHref="#"
+            linkHref="/notifications"
             linkLabel="Notifications"
             FillIcon={GoHeartFill}
             OutlineIcon={GoHeart}
+            active={pathname === "/notifications"}
           />
         </li>
 
         <li>
           <NavLink
-            linkHref="#"
+            linkHref="/messages"
             linkLabel="Messages"
             FillIcon={AiFillMessage}
             OutlineIcon={AiOutlineMessage}
+            active={pathname === "/messages"}
           />
         </li>
       </ul>
--- src/components/post/PostsGrid.tsx
+++ src/components/post/PostsGrid.tsx
@@ -1,10 +1,53 @@
+"use client";
+
 import Image from "next/image";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { useState } from "react";
 import { cn } from "@/lib/utils";
+import { GoHeartFill } from "react-icons/go";
+import { FaComment } from "react-icons/fa";
 
 type Props = {
   heightMinusOffset: number;
 };
+
+function Post({ url }: { url: string }) {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div
+      className="relative select-none"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <AspectRatio
+        ratio={1 / 1}
+        className={cn(
+          "m-[1px] cursor-pointer transition-all duration-300 hover:opacity-60",
+          hover ? "opacity-60" : "",
+        )}
+      >
+        <Image src={url} fill alt="image" />
+      </AspectRatio>
+      <div
+        className={cn(
+          "absolute left-1/2 top-1/2 z-50 hidden -translate-x-1/2 -translate-y-1/2 cursor-pointer transition-all duration-300",
+          hover ? "flex items-center gap-x-8 lg:gap-x-12" : "",
+        )}
+        onMouseEnter={() => setHover(true)}
+      >
+        <div className="flex flex-col items-center">
+          <GoHeartFill className="h-6 w-4 text-white lg:h-6 lg:w-6" />
+          <span className="text-white max-lg:text-xs">12</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <FaComment className="h-6 w-4 text-white lg:h-6 lg:w-6" />
+          <span className="text-white max-lg:text-xs">12</span>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function PostsGrid({ heightMinusOffset }: Props) {
   const imageUrl =
@@ -14,9 +57,7 @@ export default function PostsGrid({ heightMinusOffset }: Props) {
   return (
     <div className="grid w-full grid-cols-3">
       {array.map((url, index) => (
-        <AspectRatio key={index} ratio={1 / 1} className="m-[1px]">
-          <Image src={url} fill alt="image" />
-        </AspectRatio>
+        <Post key={index} url={url} />
       ))}
     </div>
   );
