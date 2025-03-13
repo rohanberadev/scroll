baa0927aea476233403be99fc73bd11d867b0b72
rohanberadev <r.beradev@gmail.com>
2025-03-13 11:55:02 +0530

    messages finished

--- src/components/message/MessageScreen.tsx
+++ src/components/message/MessageScreen.tsx
@@ -5,8 +5,8 @@ import { cn } from "@/lib/utils";
 import { type RouterOutputs, api as apiClient } from "@/trpc/react";
 import { pusher } from "@/utils/pusher/client";
 import { getPublicRoomId } from "@/utils/pusher/roomId";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useMemo, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import InputMessageBox from "./InputMessageBox";
 
 export default function MessageScreen({
@@ -24,40 +24,24 @@ export default function MessageScreen({
   };
 }) {
   type MessageType =
-    RouterOutputs["message"]["recentMessagesOfUser"]["messages"];
+    RouterOutputs["message"]["recentMessagesOfUser"]["messages"][number];
 
   const [content, setContent] = useState("");
+  const [messages, setMessages] = useState<MessageType[]>();
+  const [nextCursor, setNextCursor] =
+    useState<RouterOutputs["message"]["recentMessagesOfUser"]["nextCursor"]>();
 
-  const queryClient = useQueryClient();
-  const { data } = useQuery({
-    queryKey: ["messages", chatUser.id],
-    queryFn: async () => {
-      const response = await getRecentMessages(chatUser.id, 10, nextCursor);
-      setNextCursor(response.nextCursor);
-      return response.messages;
-    },
-    initialData: undefined,
-    enabled: Boolean(chatUser.id),
-  });
+  const { ref: inViewRef, inView } = useInView({ delay: 300 });
 
   const sendMessage = apiClient.message.create.useMutation();
   const roomId = getPublicRoomId(sessionUser.id, chatUser.id);
-  const [nextCursor, setNextCursor] =
-    useState<RouterOutputs["message"]["recentMessagesOfUser"]["nextCursor"]>();
 
   useEffect(() => {
     const channel = pusher.subscribe(roomId);
 
-    const handleNewMessage = (newMessage: MessageType[0]) => {
-      console.log("message: ", newMessage);
-      queryClient.setQueryData(
-        ["messages", chatUser.id],
-        (oldData?: MessageType) => {
-          if (!oldData) return [newMessage];
-          return [newMessage, ...oldData];
-        },
-      );
-    };
+    async function handleNewMessage(newMessage: MessageType) {
+      setMessages((prev) => (prev ? [newMessage, ...prev] : [newMessage]));
+    }
 
     channel.bind("message", handleNewMessage);
 
@@ -65,23 +49,63 @@ export default function MessageScreen({
       channel.unbind("message", handleNewMessage);
       pusher.unsubscribe(roomId);
     };
-  }, [queryClient, roomId, chatUser]);
+  }, [messages, roomId, chatUser]);
 
   useEffect(() => {
     const messageEnd = document.getElementById("messageEnd");
-    if (messageEnd) {
+    if (messageEnd && !inView) {
       messageEnd.scrollIntoView({ behavior: "smooth" });
     }
-  }, [data]);
+  }, [messages, inView]);
 
-  const messages = useMemo(() => data?.slice().reverse(), [data]);
+  useEffect(() => {
+    async function fetchMessages() {
+      try {
+        const response = await getRecentMessages(chatUser.id, 10, undefined);
+        setMessages(response.messages);
+        setNextCursor(response.nextCursor);
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+      }
+    }
+
+    fetchMessages().catch((error) => console.error(error));
+    return () => {
+      setMessages(undefined);
+      setNextCursor(undefined);
+    };
+  }, [chatUser.id]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      try {
+        const response = await getRecentMessages(chatUser.id, 10, nextCursor);
+        setMessages((prev) =>
+          prev ? [...prev, ...response.messages] : [...response.messages],
+        );
+        setNextCursor(response.nextCursor);
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+      }
+    }
+
+    if (nextCursor && chatUser.id && inView) {
+      fetchMessages().catch((error) => console.error(error));
+    }
+  }, [inView, nextCursor, chatUser.id]);
+
+  const reversedMessages = useMemo(
+    () => messages?.slice().reverse(),
+    [messages],
+  );
 
   return (
     <>
       <div className="flex min-h-full flex-col gap-y-6 p-6" id="chatScreen">
-        {messages?.map((message) => (
+        <div ref={inViewRef}></div>
+        {reversedMessages?.map((message, index) => (
           <div
-            key={message.id}
+            key={index}
             className={cn(
               "flex max-h-[1000px] w-[300px] flex-col justify-center text-wrap rounded-lg px-4 py-2",
               message.sentByUserId === sessionUser.id
